ee9e57b5759cd7af99b169270d36439efc0e2c0d
PixelNini <github@pixelnini.com>
2023-08-01 18:40:36 +0200

    feat: cleanup error handling

--- src/profile-editor.ts
+++ src/profile-editor.ts
@@ -1,29 +1,34 @@
 async function run(): Promise<void> {
-    const textArea = document.querySelector('#profileCodeForm > textarea') as HTMLTextAreaElement;
-    const characterCount = document.querySelector('#characterCount') as HTMLElement;
+    const textArea = document.querySelector<HTMLTextAreaElement>('#profileCodeForm > textarea');
+    const characterCount = document.querySelector('#characterCount');
 
     if (!textArea || !characterCount) {
         throw new Error(`No valid textarea/characterCount element found !`);
     }
 
-    render();
+    await render();
 
-    characterCount.addEventListener('DOMSubtreeModified', async () => {
-        inputValidation(textArea);
-    });
-    characterCount.dispatchEvent(new Event('DOMSubtreeModified', { bubbles: true }));
+    new MutationObserver(function(mutations) {
+        mutations.forEach(function() {
+            inputValidation(textArea);
+        });
+    }).observe(characterCount, { childList: true });
 }
 
 async function render(): Promise<void> {
     const charCountWrapper = document.querySelector('#characterCountDiv');
+    const giftButton = document.querySelector<HTMLButtonElement>('#giftButton');
 
-    if (!charCountWrapper) {
-        throw new Error(`No valid characterCountDiv found !`);
+    if (!charCountWrapper || !giftButton) {
+        throw new Error(`No valid characterCountDiv/giftButton found !`);
     }
 
     let bytesCountWrapper = document.createElement('div');
+    bytesCountWrapper.classList.add('pe-hidden');
+
     let bytesTitle = document.createElement('span');
     bytesTitle.textContent = 'Bytes';
+    bytesTitle.classList.add('pe-hidden');
 
     charCountWrapper.prepend(bytesCountWrapper);
     charCountWrapper.prepend(bytesTitle);
@@ -31,9 +36,37 @@ async function render(): Promise<void> {
     document.addEventListener('byteSizeChanged', async (event: any) => {
         const maxBytesError = event.detail.bytes > event.detail.maxBytes;
 
+        !bytesTitle.classList.contains('pe-hidden') ? showBytesError(maxBytesError) : showBytesError(maxBytesError, false);
+
         bytesCountWrapper.innerHTML = `<span class='${maxBytesError ? 'pe-warning' : ''}'>
         ${event.detail.bytes}</span> / ${event.detail.maxBytes}`;
     });
+
+    new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if(mutation.attributeName === 'disabled') {
+                bytesTitle.classList.toggle('pe-hidden');
+                bytesCountWrapper.classList.toggle('pe-hidden');
+            }
+        });    
+    }).observe(giftButton, { attributes: true });
+}
+
+async function showBytesError(maxBytesError: boolean, showError: boolean = true): Promise<void> {
+    const warningsWrapper = document.querySelector('#warnings');
+    const errorDialog = document.querySelector('#errorDialog');
+
+    if (!warningsWrapper || !errorDialog) {
+        throw new Error(`No valid warningsDiv/errorDialog found !`);
+    }
+
+    let errorMessage = document.createElement('span');
+    errorMessage.textContent = 'Too many bytes !';
+    errorMessage.classList.add('pe-warning', 'pe-block');
+
+    maxBytesError && showError ? warningsWrapper.prepend(errorMessage) : errorMessage.remove();
+
+    warningsWrapper.contains(errorMessage) ? errorDialog.classList.add('pe-block') : errorDialog.classList.remove('pe-block');
 }
 
 async function inputValidation(textArea: HTMLTextAreaElement, maxBytes: number = 255): Promise<void> {
@@ -53,5 +86,5 @@ async function inputValidation(textArea: HTMLTextAreaElement, maxBytes: number =
 }
 
 (async () => {
-    await run();
+    run();
 })();
